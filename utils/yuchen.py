f68b7baac94563ecbe96a1e731a3c0616e3685d7
superHao <2217745768@qq.com>
2023-12-31 22:41:32 +0800

    perf(yuchen.py):
    使用session连接，提高性能

--- utils/yuchen.py
+++ utils/yuchen.py
@@ -14,6 +14,7 @@ class Account:
         self.username = account["username"]
         self.password = account["password"]
         self.user_agent = account["user_agent"]
+        self.session = requests.session()
         self.cookie = None
         self.token = self.get_token()
         log.debug(self.__str__())
@@ -30,8 +31,7 @@ class Account:
             "User-Agent": self.user_agent
         }
 
-    @staticmethod
-    def get_token() -> str:
+    def get_token(self) -> str:
         """
         获取登录所需token \n
         无token不能通过安全效验
@@ -41,7 +41,7 @@ class Account:
         headers = {
             "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0"
         }
-        response = requests.get(url=url, headers=headers)
+        response = self.session.get(url=url, headers=headers)
         soup = BeautifulSoup(response.text, "html.parser")
         token = soup.find('input', {'name': 'token'}).get('value')
         log.debug(f"token:{token}")
@@ -49,7 +49,7 @@ class Account:
 
     def yuchen_login(self):
         """
-        登录网站获取cookie
+        登录网站
         """
         log.info("开始登录")
         url = "https://yuchen.tonghuaios.com/wp-admin/admin-ajax.php"
@@ -69,14 +69,14 @@ class Account:
         #     "User-Agent": self.user_agent
         # }
         headers = self.headers()
-        response = requests.post(url=url, data=data, headers=headers)
+        response = self.session.post(url=url, data=data, headers=headers)
         log.debug(response.json())
         message = LoginResultHandler(response.json())
         if message.success == "error":
             log.info("登录失败")
             log.info(message.msg)
             return False
-        self.cookie = response.cookies
+        # self.cookie = response.cookies
         return True
 
     def yuchen_check(self):
@@ -91,7 +91,8 @@ class Account:
             "action": "daily_sign"
         }
         headers = self.headers()
-        response = requests.post(url=url, data=data, headers=headers, cookies=self.cookie)
+        # response = requests.post(url=url, data=data, headers=headers, cookies=self.cookie)
+        response = self.session.post(url=url, data=data, headers=headers)
         log.debug(response.json())
         message = LoginResultHandler(response.json())
         log.info(message.msg)
@@ -103,7 +104,8 @@ class Account:
         """
         url = "https://yuchen.tonghuaios.com/users?tab=credit"
         headers = self.headers()
-        response = requests.get(url=url, headers=headers, cookies=self.cookie)
+        # response = self.session.get(url=url, headers=headers, cookies=self.cookie)
+        response = self.session.get(url=url, headers=headers)
         soup = BeautifulSoup(response.text, "html.parser")
         text = soup.find('div', {'class': 'header_tips'}).text
         log.info(text)
