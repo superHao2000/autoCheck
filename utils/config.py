e36c53b215bdea3321110ea65d9fcde918b76063
superHao <2217745768@qq.com>
2025-01-25 11:43:19 +0800

    feat:配置文件不存在时从备份重新生成

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,5 @@
 import os.path
+import shutil
 import sys
 from json import JSONDecodeError
 
@@ -11,9 +12,10 @@ ROOT_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 """主目录"""
 CONFIG_PATH = os.path.join(ROOT_PATH, 'config.yaml')
 """数据文件目录"""
+CONFIG_PATH_BAK = os.path.join(ROOT_PATH, 'config.yaml.bak')
 
 
-def read_data():
+def read_data() -> dict:
     try:
         with open(CONFIG_PATH, 'r') as file:
             config = yaml.safe_load(file)
@@ -27,13 +29,28 @@ def read_data():
         raise
 
 
+def write_data() -> None:
+    source = CONFIG_PATH_BAK
+    destination = CONFIG_PATH
+    try:
+        # 检查源文件是否存在
+        if os.path.exists(source):
+            # 复制并重命名文件
+            shutil.copyfile(source, destination)
+            log.info(f"配置文件 {source} 已成功生成")
+        else:
+            log.info(f"配置文件备份 {source} 不存在。")
+    except Exception as e:
+        log.debug(f"发生错误: {e}")
+
+
 if os.path.exists(CONFIG_PATH):
     Conf = read_data()
     ACCOUNT = Conf.get('ACCOUNT')
     PUSH = Conf.get('PUSH')
     USER_AGENT = Conf.get('USER_AGENT')
 else:
-    log.info("配置文件不存在")
+    write_data()
     log.info("请填写配置文件后重新启动")
     sys.exit()
 
